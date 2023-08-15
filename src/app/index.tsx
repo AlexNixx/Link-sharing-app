517745c5f5e0de3c6452beda7ba6eea3b61ce57d
AlexNix <alexnix.mail@gmail.com>
2023-08-15 22:22:32 +0300

    set up: webpack, ts, and project schema

---
+++ src/app/index.tsx
@@ -0,0 +1,30 @@
+import {Link, Route, Routes} from "react-router-dom";
+
+import Home from "../pages/Home";
+import Some from "../pages/Some";
+
+import "./styles/index.scss"
+
+import {useTheme} from "./theme/useTheme";
+import {classNames} from "./shared/lib/classNames/classNames";
+
+const App = () => {
+    const {theme, toggleTheme} = useTheme();
+
+    return (
+        <div className={classNames("app", {}, [theme])}>
+            <nav>
+                <Link to={"/"}>Home</Link>
+                <Link to={"/some"}>Some</Link>
+                <button onClick={toggleTheme}>Change theme</button>
+            </nav>
+            <Routes>
+                <Route path={"/"} element={<Home/>}/>
+                <Route path={"/some"} element={<Some/>}/>
+                <Route path={"/*"} element={<Some/>}/>
+            </Routes>
+        </div>
+    )
+}
+
+export default App;
---
+++ src/pages/Some.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Some = () => {
+    return (
+        <div>SomePage</div>
+    )
+};
+
+export default Some;
